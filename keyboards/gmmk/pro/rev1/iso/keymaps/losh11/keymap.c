7a655f99d2cf11872fae02d34fbe2c5233f59a7d
Loshan T <losh11@users.noreply.github.com>
2024-06-13 01:57:24 +0100

    Update keymap.c

--- keyboards/gmmk/pro/rev1/iso/keymaps/losh11/keymap.c
+++ keyboards/gmmk/pro/rev1/iso/keymaps/losh11/keymap.c
@@ -13,17 +13,18 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 #include QMK_KEYBOARD_H
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-//      ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12	     §             Rotary(Mute)
+//      ESC      F1       F2       F3       F4       F5       F6       F7       F8       F9       F10      F11      F12	     Prt           Rotary(Mute)
 //      ~        1        2        3        4        5        6        7        8        9        0         -       (=)	     BackSpc           Del
 //      Tab      Q        W        E        R        T        Y        U        I        O        P        [        ]                          PgUp
 //      Caps     A        S        D        F        G        H        J        K        L        ;        "        #        Enter             PgDn
 //      Sh_L     /        Z        X        C        V        B        N        M        ,        .        ?                 Sh_R     Up       End
-//      Win_L    Alt_L    Ct_L                                SPACE                               Ct_R    FN        Alt_R    Left     Down     Right
+//      Win_L    Alt_L    Ct_L                                SPACE                               Ct_R     FN       Alt_R    Left     Down     Right
 
 
     // The FN key by default maps to a momentary toggle to layer 1 to provide access to the QK_BOOT key (to put the board into bootloader mode). Without
@@ -38,12 +39,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Since this is, among other things, a "gaming" keyboard, a key combination to enable NKRO on the fly is provided for convenience.
     // Press Fn+N to toggle between 6KRO and NKRO. This setting is persisted to the EEPROM and thus persists between restarts.
     [0] = LAYOUT(
-        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  UC(0xa7),         KC_MUTE,
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,                   KC_PGUP,
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_NUHS, KC_ENT,           KC_PGDN,
         KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
-        KC_LGUI, KC_LALT, KC_LCTL,                            KC_SPC,                             KC_RCTL, MO(1),   KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_LGUI, KC_LALT, KC_LCTL                             KC_SPC,                             KC_RCTL, MO(1),   KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [1] = LAYOUT(
@@ -60,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // clang-format on
 
 #if defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
     [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [1] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) }
 };
@@ -70,23 +71,23 @@ const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     if (host_keyboard_led_state().caps_lock) {
         // turn on all left-side LEDs
-        rgb_matrix_set_color(68, RGB_SPRINGGREEN);
-        rgb_matrix_set_color(71, RGB_SPRINGGREEN);
-        rgb_matrix_set_color(74, RGB_SPRINGGREEN);
-        rgb_matrix_set_color(77, RGB_SPRINGGREEN);
-        rgb_matrix_set_color(81, RGB_SPRINGGREEN);
-        rgb_matrix_set_color(84, RGB_SPRINGGREEN);
-        rgb_matrix_set_color(88, RGB_SPRINGGREEN);
-        rgb_matrix_set_color(92, RGB_SPRINGGREEN);
+        rgb_matrix_set_color(68, RGB_RED);
+        rgb_matrix_set_color(71, RGB_RED);
+        rgb_matrix_set_color(74, RGB_RED);
+        rgb_matrix_set_color(77, RGB_RED);
+        rgb_matrix_set_color(81, RGB_RED);
+        rgb_matrix_set_color(84, RGB_RED);
+        rgb_matrix_set_color(88, RGB_RED);
+        rgb_matrix_set_color(92, RGB_RED);
         // turn on all right-side LEDs
-        rgb_matrix_set_color(69, RGB_SPRINGGREEN);
-        rgb_matrix_set_color(72, RGB_SPRINGGREEN);
-        rgb_matrix_set_color(75, RGB_SPRINGGREEN);
-        rgb_matrix_set_color(78, RGB_SPRINGGREEN);
-        rgb_matrix_set_color(82, RGB_SPRINGGREEN);
-        rgb_matrix_set_color(85, RGB_SPRINGGREEN);
-        rgb_matrix_set_color(89, RGB_SPRINGGREEN);
-        rgb_matrix_set_color(93, RGB_SPRINGGREEN);
+        rgb_matrix_set_color(69, RGB_RED);
+        rgb_matrix_set_color(72, RGB_RED);
+        rgb_matrix_set_color(75, RGB_RED);
+        rgb_matrix_set_color(78, RGB_RED);
+        rgb_matrix_set_color(82, RGB_RED);
+        rgb_matrix_set_color(85, RGB_RED);
+        rgb_matrix_set_color(89, RGB_RED);
+        rgb_matrix_set_color(93, RGB_RED);
     }
     return false;
 }
